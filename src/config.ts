fddcff55ba3a676438c33e278979af3afdebb394
lzhabo <lidia.zhabo99@gmail.com>
2023-04-28 11:27:31 +0300

    added deploy file and change variable name from CONTRACT_ADDRESSES to CONTRACT_ADDRESS

--- src/config.ts
+++ src/config.ts
@@ -7,5 +7,5 @@ config({ path: path.join(__dirname, "../.env") });
 export const mongoUrl = loadVar("MONGO_URL");
 export const port = loadVar("PORT", true);
 export const privateKey = loadVar("PRIVATE_KEY");
-export const contractAddress = loadVar("CONTRACT_ADDRESSES");
+export const contractAddress = loadVar("CONTRACT_ADDRESS");
 export const nodeUrl = loadVar("NODE_URL");
